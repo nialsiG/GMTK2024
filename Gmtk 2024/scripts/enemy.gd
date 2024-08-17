f521086af412392914ea415ddbfc6195042645e2
Ghislain Thiery <ghislain.thiery@ntymail.com>
2024-08-17 02:35:15 +0200

    deinogalerix movement

--- Gmtk 2024/scripts/enemy.gd
+++ Gmtk 2024/scripts/enemy.gd
@@ -1,11 +1,45 @@
 class_name Enemy extends Animal
 
+enum state { IDLE, MOVING, CHASING, ATTACKING, FLEEING, WOUNDED, DEAD }
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var current_state = state.IDLE
+@onready var timer = 0
 
+@export var MAX_IDLE_TIME: float
+@export var MAX_MOVE_TIME: float
+@export var MAX_FLEEING_TIME: float
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	match current_state:
+		state.IDLE:
+			timer += delta
+			if timer > MAX_IDLE_TIME:
+				timer = 0
+				current_state = state.MOVING
+				start_moving()
+		state.MOVING:
+			timer += delta
+			if timer > MAX_MOVE_TIME:
+				timer = 0
+				current_state = state.IDLE
+				axis = Vector2.ZERO
+		state.CHASING:
+			pass
+		state.ATTACKING:
+			pass
+		state.FLEEING:
+			pass
+		state.WOUNDED:
+			pass
+		state.DEAD:
+			pass
+		_:
+			pass
+	move(delta)
+
+func _physics_process(delta):
+	move_and_slide()
+
+func start_moving():
+	var my_array = [Vector2(1,0), Vector2(1,1), Vector2(0,1), Vector2(-1,1), Vector2(-1,0), Vector2(-1,-1), Vector2(0,-1), Vector2(1,-1)]
+	axis = my_array.pick_random()
