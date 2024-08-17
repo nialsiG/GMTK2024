f6b75b8ed2f246ff255f89463d508fff7602199c
Ghislain Thiery <ghislain.thiery@ntymail.com>
2024-08-17 02:08:13 +0200

    add player movement

--- Gmtk 2024/scripts/player.gd
+++ Gmtk 2024/scripts/player.gd
@@ -1,11 +1,23 @@
 class_name Player extends Animal
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	get_input_axis()
+	#debug change size
+	if Input.is_action_just_pressed("size_up"):
+		increase_size()
+	if Input.is_action_just_pressed("size_down"):
+		decrease_size()
+	move(delta)
+
+func _physics_process(delta):
+	move_and_slide()
+
+func get_input_axis():
+	axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	axis.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+	if axis != Vector2.ZERO:
+		print("You are moving!")
