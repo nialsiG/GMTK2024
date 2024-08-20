ec32c271b5922c5046049e4fc833ff6e0b574d16
Lygaenth <115181383+Lygaenth@users.noreply.github.com>
2024-08-20 22:32:55 +0200

    Fix: Start menu: removed white square on start button. Running hamster is not displayed on going back to start menu

--- Gmtk 2024/scripts/start_menu.gd
+++ Gmtk 2024/scripts/start_menu.gd
@@ -3,14 +3,15 @@ extends Node
 @onready var game = "res://scenes/world.tscn"
 @onready var focus_button = $CanvasLayer/VBoxContainer/start_button
 @onready var startGoosePlayer : AudioStreamPlayer = $StartGoosePlayer
-@onready var loadingSprite : AnimatedSprite2D = $CanvasLayer/AnimatedSprite2D
+@onready var _loadingSprite : AnimatedSprite2D = $CanvasLayer/AnimatedSprite2D
 func _ready():
 	focus_button.grab_focus()
+	_loadingSprite.hide()
 
 func _on_start_button_pressed():
 	focus_button.disabled = true
 	startGoosePlayer.play()
-	loadingSprite.show()
+	_loadingSprite.show()
 	await get_tree().create_timer(1.5).timeout
 	get_tree().change_scene_to_file(game)
 
