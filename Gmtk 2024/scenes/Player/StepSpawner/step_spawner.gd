64b7ada3a3c69c966697f23a351b82a98e5f59ab
Lygaenth <115181383+Lygaenth@users.noreply.github.com>
2025-01-21 23:47:46 +0100

    Feature: Updated steps to switch between two (bad) sprites

--- Gmtk 2024/scenes/Player/StepSpawner/step_spawner.gd
+++ Gmtk 2024/scenes/Player/StepSpawner/step_spawner.gd
@@ -3,7 +3,10 @@ class_name StepSpawner
 
 const enums = preload("res://scripts/enums.gd")
 const stepSpritePs = preload("res://scenes/Player/StepSpawner/StepSprite.tscn")
-const _groundSprite = preload("res://assets/sprites/Player/ground_step.png")
+const _groundSprite1 = preload("res://assets/sprites/Player/ground_step1.png")
+const _groundSprite2 = preload("res://assets/sprites/Player/ground_step2.png")
+var _textureCounter = 0;
+var _textureCount = 2;
 
 signal SpawnStep(sprite : StepSprite)
 
@@ -13,6 +16,15 @@ func Initialize(player : Player):
 func OnStepSpawn(stepType : enums.StepType, globalPosition : Vector2):
 	var step : Sprite2D
 	var newStep = stepSpritePs.instantiate()
-	newStep.texture = _groundSprite
+	newStep.texture = GetTexture()
 	newStep.global_position = globalPosition
 	SpawnStep.emit(newStep)
+
+func GetTexture() -> Texture2D :
+	_textureCounter = (_textureCounter + 1) % _textureCount
+	if (_textureCounter == 0):
+		return _groundSprite1
+	elif (_textureCounter == 1):
+		return _groundSprite2
+		
+	return _groundSprite1
